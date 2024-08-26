b093e7ce0341a0248d232252093548de93ee3423
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-26 22:47:59 +0530

    variables

--- 06-variable.sh
+++ 06-variable.sh
@@ -2,5 +2,6 @@
 echo "Please enter your username::"
 read -s USERNAME
 echo "Username entered is: $USERNAME"
+echo "Please enter your password::"
 read -s PASSWORD
 echo "Password enter is:: $PASSWORD"
