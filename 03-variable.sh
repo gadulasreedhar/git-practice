83b582317148768a516acc9a731fce092ae123b2
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-28 10:59:00 +0530

    modifed

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERSON1=Ramesh
 PERSON2=Suresh
-echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON1:: Hi ${PERSON2}, how are you?"
 echo "$PERSON2:: Hello $PERSON1,I am fine.How are you doing?"
 echo "$PERSON1:: I am doing good $PERSON2.Whats going on "
 echo "$PERSON2:: I started learning shell script $PERSON1"
