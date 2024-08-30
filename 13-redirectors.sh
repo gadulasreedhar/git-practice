5b045aaa536e7dbffb8363b4c7c2267f4e5bc75d
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-30 22:08:49 +0530

    Redirector

--- 13-redirectors.sh
+++ 13-redirectors.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-R="/e[31m"
-G="/e[32m"
-N="/e[0m
-
-CHKROOT(){
-if [ $USRDID -ne 0]
-then
-    echo "Please run this script with root privilage"
-    exit 1
-fi
-}
-
-CHKROOT
-
-
+USEDID=$(id -u)
+echo "$USERID"
