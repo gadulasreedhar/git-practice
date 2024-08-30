4c1412860de1309418d445eb3773e8bb691c8c40
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-30 22:35:45 +0530

    Redirector

--- 13-redirectors.sh
+++ 13-redirectors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
 
-# R="\e[31m"
-# G="\e[32m"
-# N="\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m
 
 
 CHKROOT(){
@@ -19,10 +19,10 @@ fi
 VALIDATE(){
 if [ $1 -ne 0 ] 
 then 
-    echo  "$2 is ... FAILED"
+    echo -e  "$2 is ... $R FAILED $N"
     exit 1
 else
-    echo -e "$2 is ... SUCCESS"
+    echo -e "$2 is ...  $G SUCCESS $N"
 fi
 }
 
@@ -39,3 +39,13 @@ else
     echo "GIT already installed nothing to do"
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing mysql"
+else
+    echo "mysql already installed nothing to do"
+fi
