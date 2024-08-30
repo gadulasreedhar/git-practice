eb9d6f302524a1fc7d92612f149df00912687111
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-30 22:20:03 +0530

    Redirector

--- 13-redirectors.sh
+++ 13-redirectors.sh
@@ -9,3 +9,25 @@ then
 else
     echo "Used id is: $USERID"
 fi
+
+VALIDATE(){
+if [ $1 -ne 0 ] 
+then 
+    echo "$2 is ...FAILED"
+    exit 1
+else
+    echo "$2 is ...SUCCESS"
+fi
+}
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    echo "GIT is not installed going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing GIT"
+else
+    echo "GIT already installed nothing to do"
+fi
+
