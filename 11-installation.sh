bb02de440a3ecc40c68867ea3f53788350cd7c10
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-30 07:03:50 +0530

    Installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo $USERID
+#echo $USERID
+
 if [ $USERID -ne 0 ]
 then
  echo "Please run this script with root privilages"
  exit 1
 fi
 
-dnf install git -y
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed goint to install it.."
+    dnf install git -y
+else
+    echo "Git already installed nothing to do"
+fi
