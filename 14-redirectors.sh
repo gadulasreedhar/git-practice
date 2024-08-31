d79e78398d6bb4c2c4520fe94c6b4adeae891d74
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-31 11:07:48 +0530

    Redirector

--- 14-redirectors.sh
+++ 14-redirectors.sh
@@ -16,7 +16,7 @@ Y="\e[33m"
 CHKROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root privilages. $N" | tee -a >>$LOG_FILE
+        echo -e "$R Please run this script with root privilages. $N" | tee -a $LOG_FILE
         exit 1 
     # else
     #     echo "Used id is: $USERID"
@@ -25,10 +25,10 @@ CHKROOT(){
 VALIDATE(){
 if [ $1 -ne 0 ] 
 then 
-    echo -e  "$2 is ... $R FAILED $N" | tee -a >>$LOG_FILE
+    echo -e  "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
     exit 1
 else
-    echo -e "$2 is ...  $G SUCCESS $N" | tee -a >>$LOG_FILE
+    echo -e "$2 is ...  $G SUCCESS $N" | tee -a $LOG_FILE
 fi
 }
 USAGE(){
@@ -36,7 +36,7 @@ USAGE(){
     exit 1
 }
 
-echo "Scipt started executed at:$(date)"  | tee -a >>$LOG_FILE
+echo "Scipt started executed at:$(date)"  | tee -a $LOG_FILE
 
 CHKROOT
 
@@ -50,10 +50,10 @@ do
 dnf list installed $package &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "$package is not installed going to install it.." &>>$LOG_FILE | tee -a >>$LOG_FILE
+    echo "$package is not installed going to install it.." &>>$LOG_FILE | tee -a $LOG_FILE
     dnf install $package -y &>>$LOG_FILE
     VALIDATE $? "Installing $package"
 else
-    echo -e "$package already $Y installed nothing to do $N" &>> $LOG_FILE | tee -a >>$LOG_FILE
+    echo -e "$package already $Y installed nothing to do $N" &>> $LOG_FILE | tee -a $LOG_FILE
 fi
 done
