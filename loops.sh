82d51d35163ac93af0a3dc015716dc8fab526580
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-31 07:14:32 +0530

    Redirector

--- loops.sh
+++ loops.sh
@@ -30,13 +30,12 @@ CHKROOT
 
 for package in $@
 do
-# dnf list installed $package
-# if [ $? -ne 0 ]
-# then
-#     echo "package is not installed going to install it.."
-#     dnf install $package -y
-#     VALIDATE $? "Installing $package"
-# else
-#     echo "$package already installed nothing to do"
-echo "$package"
+dnf list installed $package
+if [ $? -ne 0 ]
+then
+    echo "$package is not installed going to install it.."
+    dnf install $package -y
+    VALIDATE $? "Installing $package"
+else
+    echo "$package already installed nothing to do"
 done
