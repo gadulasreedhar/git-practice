37f6beaee06c69ee63b8d4627dccc83c5b30d4d8
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-31 10:25:16 +0530

    Redirector

--- 14-redirectors.sh
+++ 14-redirectors.sh
@@ -51,6 +51,7 @@ then
     dnf install $package -y &>>$LOG_FILE
     VALIDATE $? "Installing $package"
 else
+    echo $LOG_FILE
     echo -e "$package already $Y installed nothing to do $N" &>> $LOG_FILE
 fi
 done
