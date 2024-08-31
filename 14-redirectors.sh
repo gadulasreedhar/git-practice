141a62a0b4038fc21956a5773984b4cad008823b
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-31 09:38:21 +0530

    Redirector

--- 14-redirectors.sh
+++ 14-redirectors.sh
@@ -15,7 +15,7 @@ N="\e[0m"
 CHKROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root privilages. $N" &>> $LOG_FILE
+        echo -e "$R Please run this script with root privilages. $N" &>>$LOG_FILE
         exit 1 
     else
         echo "Used id is: $USERID"
@@ -34,15 +34,15 @@ fi
 
 CHKROOT
 
-for package in $@
-do
-dnf list installed $package
-if [ $? -ne 0 ]
-then
-    echo "$package is not installed going to install it.."
-    dnf install $package -y
-    VALIDATE $? "Installing $package"
-else
-    echo "$package already installed nothing to do"
-fi
-done
+# for package in $@
+# do
+# dnf list installed $package
+# if [ $? -ne 0 ]
+# then
+#     echo "$package is not installed going to install it.."
+#     dnf install $package -y
+#     VALIDATE $? "Installing $package"
+# else
+#     echo "$package already installed nothing to do"
+# fi
+# done
