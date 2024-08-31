969278a85cda658b5858c252e884e4a0eff350df
gadulasreedhar <gadulasreedhar@gmail.com>
2024-08-31 09:51:59 +0530

    Redirector

--- 14-redirectors.sh
+++ 14-redirectors.sh
@@ -22,4 +22,14 @@ CHKROOT(){
         echo "Used id is: $USERID"
    fi
 }
+VALIDATE(){
+if [ $1 -ne 0 ] 
+then 
+    echo -e  "$2 is ... $R FAILED $N"
+    exit 1
+else
+    echo -e "$2 is ...  $G SUCCESS $N"
+fi
+}
+
 CHKROOT
